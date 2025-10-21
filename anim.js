2b9e940cff5e5aecc897cac37d8bccea72e9f907
arthurdasler <arthurdasler@gmail.com>
2025-10-21 16:55:07 +0300

    1

--- anim.js
+++
@@ -1,8 +0,0 @@
-  document.addEventListener("DOMContentLoaded", () => {
-    gsap.to("#page-content", {
-      opacity: 1,
-      y: 0,
-      duration: 0.3,
-      ease: "power2.out"
-    });
-  });
